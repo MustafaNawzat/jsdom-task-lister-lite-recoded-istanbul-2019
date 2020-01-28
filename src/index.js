648106dfa6ad265bcc5b41e470c5443ac3320804
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-01-28 16:20:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
       document.getElementById("submitId").addEventListener("click", function(event) {
-    event.preventDefault();
-    let li = document.createElement("li"); // create an li
-    li.innerText = document.getElementById("inputId").value;
-    document.getElementById("tasks").appendChild(li);
-    document.getElementById("inputId").value=' ';
-  });
+      event.preventDefault();
+      let li = document.createElement("li"); // create an li
+      li.innerText = document.getElementById("inputId").value;
+      document.getElementById("tasks").appendChild(li);
+      document.getElementById("inputId").value=' ';
+    });
 });
